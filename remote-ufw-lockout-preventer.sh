05919acc2202acde2e2a24b7d7029f8391dc9007
damola adebayo <adebayo10k@gmail.com>
2021-07-11 14:09:57 +0100

    adds sourced constants/regexes library

--- remote-ufw-lockout-preventer.sh
+++ remote-ufw-lockout-preventer.sh
@@ -13,6 +13,67 @@
 # the cronjob can be commented out or removed when testing is complete.
 #===================================================
 
+##################################################################
+##################################################################
+# THIS STUFF IS HAPPENING BEFORE MAIN FUNCTION CALL:
+#===================================
+
+# 1. MAKE SHARED LIBRARY FUNCTIONS AVAILABLE HERE
+
+# make all those library function available to this script
+shared_bash_functions_fullpath="${SHARED_LIBRARIES_DIR}/shared-bash-functions.sh"
+shared_bash_constants_fullpath="${SHARED_LIBRARIES_DIR}/shared-bash-constants.inc.sh"
+
+for resource in "$shared_bash_functions_fullpath" "$shared_bash_constants_fullpath"
+do
+	if [ -f "$resource" ]
+	then
+		echo "Required library resource FOUND OK at:"
+		echo "$resource"
+		source "$resource"
+	else
+		echo "Could not find the required resource at:"
+		echo "$resource"
+		echo "Check that location. Nothing to do now, except exit."
+		exit 1
+	fi
+done
+
+
+# 2. MAKE SCRIPT-SPECIFIC FUNCTIONS AVAILABLE HERE
+
+# must resolve canonical_fullpath here, in order to be able to include sourced function files BEFORE we call main, and  outside of any other functions defined here, of course.
+
+# at runtime, command_fullpath may be either a symlink file or actual target source file
+command_fullpath="$0"
+command_dirname="$(dirname $0)"
+command_basename="$(basename $0)"
+
+# if a symlink file, then we need a reference to the canonical file name, as that's the location where all our required source files will be.
+# we'll test whether a symlink, then use readlink -f or realpath -e although those commands return canonical file whether symlink or not.
+# 
+canonical_fullpath="$(readlink -f $command_fullpath)"
+canonical_dirname="$(dirname $canonical_fullpath)"
+
+# this is just development debug information
+if [ -h "$command_fullpath" ]
+then
+	echo "is symlink"
+	echo "canonical_fullpath : $canonical_fullpath"
+else
+	echo "is canonical"
+	echo "canonical_fullpath : $canonical_fullpath"
+fi
+
+# included source files for json profile import functions
+#source "${canonical_dirname}/preset-profile-builder.inc.sh"
+
+
+# THAT STUFF JUST HAPPENED BEFORE MAIN FUNCTION CALL!
+##################################################################
+##################################################################
+
+
 function main 
 {
 ################################################################
